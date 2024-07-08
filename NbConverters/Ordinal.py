7e43f0670fe0f1cc6fe1b68d45756fd7457ffeb2
Lemoi18 <lemoi18@uia.no>
2024-07-08 00:15:55 +0000

    fix in Ordinal.py and fix Date.py

--- NbConverters/Ordinal.py
+++ NbConverters/Ordinal.py
@@ -1,8 +1,6 @@
 from singleton_decorator import singleton
 import re
-from Roman import Roman
 from Cardinal import Cardinal
-
 
 @singleton
 class Ordinal:
@@ -14,7 +12,6 @@ class Ordinal:
         self.filter_regex = re.compile(r"[, ºª]")
         self.standard_case_regex = re.compile(r"(?i)(\d+)(a|e|ende|nde|ste)?")
         self.cardinal = Cardinal()
-        self.roman = Roman()
         self.trans_denominator = {
             "null": "nullte",
             "en": "første",
@@ -39,7 +36,8 @@ class Ordinal:
             "nitten": "nittende",
             "tjue": "tjuende",
             "tjueen": "tjueførste",
-            "tjueto": "tjuandre",
+            "tjueett": "tjueførste",
+            "tjueto": "tjueandre",
             "tjuetre": "tjuetredje",
             "tjuefire": "tjuefjerde",
             "tjuefem": "tjuefemte",
@@ -48,6 +46,7 @@ class Ordinal:
             "tjueåtte": "tjueåttende",
             "tjueni": "tjueniende",
             "tretti": "trettiende",
+            "trettien": "trettiførste",
             "førti": "førtiende",
             "femti": "femtiende",
             "seksti": "sekstiende",
@@ -62,31 +61,11 @@ class Ordinal:
         }
     
     def convert(self, token: str) -> str:
-        # 1. Filtrer ut kommaer, mellomrom og ordinalindikatorer.
         token = self.filter_regex.sub("", token)
-
-        prefix = ""
-
-        # 2. Sjekk for romertall, og konverter til heltall som streng hvis det er tilfelle.
-        if self.cardinal.roman.check_if_roman(token):
-            # 3. Hvis det er tilfelle, sett prefiks til "den".
-            token = self.cardinal.roman.convert(token)
-            prefix = "den"
-        else:
-            # 4. Hvis ikke, fjern potensielt ordinal-suffiks ("a", "e", "ende", "nde" eller "ste").
-            match = self.standard_case_regex.fullmatch(token)
-            if match:
-                token = match.group(1)
-
-        # 5. Konverter det gjenværende heltallet som streng til kardinaltall, 
-        # og erstatt det siste ordet med et ord i ordinal stil.
+        match = self.standard_case_regex.fullmatch(token)
+        if match:
+            token = match.group(1)
         number_text_list = self.cardinal.convert(token).split(" ")
         number_text_list[-1] = self.trans_denominator.get(number_text_list[-1], number_text_list[-1])
-        result = " ".join(number_text_list)
-
-        # 6. Bruk prefiks hvis nødvendig.
-        if prefix:
-            result = f"{prefix} {result}"
-
-        return result
+        return " ".join(number_text_list)
 
